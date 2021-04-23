16a21877f0fa5cefe97c92368d04c19420f96e2d
melike2d <ML2DOfficial@gmail.com>
2021-04-22 21:15:43 -0700

    :children_crossing: improve scheduler

--- src/main/kotlin/me/devoxin/flight/internal/utils/Scheduler.kt
+++ src/main/kotlin/me/devoxin/flight/internal/utils/Scheduler.kt
@@ -3,11 +3,27 @@ package me.devoxin.flight.internal.utils
 import java.util.concurrent.Executors
 import java.util.concurrent.ScheduledFuture
 import java.util.concurrent.TimeUnit
+import kotlin.concurrent.thread
 
 object Scheduler {
-  private val schd = Executors.newSingleThreadScheduledExecutor()
+  @PublishedApi
+  internal val executor = Executors.newSingleThreadScheduledExecutor {
+    thread(name = "Flight Scheduler", isDaemon = true) { it.run() }
+  }
 
-  fun every(milliseconds: Long, block: () -> Unit): ScheduledFuture<*> {
-    return schd.scheduleAtFixedRate(block, milliseconds, milliseconds, TimeUnit.MILLISECONDS)
+  /**
+   * Calls [block] every [delay].
+   *
+   * @param delay
+   *   How long to wait before each execution.
+   *
+   * @param timeUnit
+   *   Time unit used for [delay]
+   *
+   * @param block
+   *   The block to call.
+   */
+  fun every(delay: Long, timeUnit: TimeUnit = TimeUnit.MILLISECONDS, block: () -> Unit): ScheduledFuture<*> {
+    return executor.scheduleAtFixedRate(block, delay, delay, timeUnit)
   }
 }
