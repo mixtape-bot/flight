22686a4680d0d3c486d6d46cdb9e0e37c4ac60d2
melike2d <gino@dimensional.fun>
2021-08-21 01:24:29 -0700

    :sparkles: make the Member & User parser retrieve from rest if not in cache

--- src/main/kotlin/me/devoxin/flight/internal/parsers/MemberParser.kt
+++ src/main/kotlin/me/devoxin/flight/internal/parsers/MemberParser.kt
@@ -1,5 +1,6 @@
 package me.devoxin.flight.internal.parsers
 
+import kotlinx.coroutines.future.await
 import me.devoxin.flight.api.Context
 import net.dv8tion.jda.api.entities.Member
 import java.util.*
@@ -9,7 +10,8 @@ class MemberParser : Parser<Member> {
     override suspend fun parse(ctx: Context, param: String): Optional<Member> {
         val snowflake = SnowflakeParser.parse(ctx, param)
 
-        member = if (snowflake.isPresent) {
+        /* firstly, test the cache. */
+        var member: Member? = if (snowflake.isPresent) {
             ctx.guild?.getMemberById(snowflake.get().resolved)
         } else {
             if (param.length > 5 && param[param.length - 5].toString() == "#") {
@@ -17,6 +19,15 @@ class MemberParser : Parser<Member> {
                 ctx.guild?.memberCache?.find { it.user.name == tag[0] && it.user.discriminator == tag[1] }
             } else {
                 ctx.guild?.getMembersByName(param, false)?.firstOrNull()
+            }
+        }
+
+        /* finally, try to retrieve the voice channel through rest. */
+        if (member == null && snowflake.isPresent) {
+            snowflake.get().resolved.runCatching {
+                member = ctx.guild?.retrieveMemberById(this)
+                    ?.submit()
+                    ?.await()
             }
         }
 
--- src/main/kotlin/me/devoxin/flight/internal/parsers/UserParser.kt
+++ src/main/kotlin/me/devoxin/flight/internal/parsers/UserParser.kt
@@ -1,16 +1,17 @@
 package me.devoxin.flight.internal.parsers
 
+import kotlinx.coroutines.future.await
 import me.devoxin.flight.api.Context
 import net.dv8tion.jda.api.entities.User
 import java.util.*
 
 class UserParser : Parser<User> {
-
     // TODO: Check ctx.message.mentionedUsers
     override suspend fun parse(ctx: Context, param: String): Optional<User> {
         val snowflake = SnowflakeParser.parse(ctx, param)
 
-        val user = if (snowflake.isPresent) {
+        /* firstly, test the cache. */
+        var user = if (snowflake.isPresent) {
             ctx.jda.getUserById(snowflake.get().resolved)
         } else {
             if (param.length > 5 && param[param.length - 5].toString() == "#") {
@@ -18,6 +19,15 @@ class UserParser : Parser<User> {
                 ctx.jda.userCache.find { it.name == tag[0] && it.discriminator == tag[1] }
             } else {
                 ctx.jda.userCache.find { it.name == param }
+            }
+        }
+
+        /* finally, see if we can retrieve by rest. */
+        if (user == null && snowflake.isPresent) {
+            snowflake.get().resolved.runCatching {
+                user = ctx.jda.retrieveUserById(this)
+                    .submit()
+                    .await()
             }
         }
 
